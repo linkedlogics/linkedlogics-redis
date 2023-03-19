049305ef43352acf9d3ec64bf192b0d56d62979c
radjab <radjab@gmail.com>
2023-03-19 08:58:23 +0100

    using consumer and publisher from framework

--- src/main/java/dev/linkedlogics/redis/service/RedisServiceConfigurer.java
+++ src/main/java/dev/linkedlogics/redis/service/RedisServiceConfigurer.java
@@ -1,15 +1,14 @@
 package dev.linkedlogics.redis.service;
 
 import dev.linkedlogics.service.ServiceConfigurer;
+import dev.linkedlogics.service.local.QueueSchedulerService;
 
 public class RedisServiceConfigurer extends ServiceConfigurer {
 	public RedisServiceConfigurer() {
 		configure(new RedisContextService());
 		configure(new RedisQueueService());
 		configure(new RedisTopicService());
-		configure(new RedisConsumerService());
-		configure(new RedisPublisherService());
-		configure(new RedisSchedulerService());
+		configure(new QueueSchedulerService());
 		configure(new RedisTriggerService());
 	}
 }
