d31bc7eb388b38d02b29f61b8bba949d652ded4d
radjab <radjab@gmail.com>
2023-03-30 01:37:24 +0200

    redis services provider impl

---
+++ src/main/java/dev/linkedlogics/redis/RedisServices.java
@@ -0,0 +1,28 @@
+package dev.linkedlogics.redis;
+
+import java.util.List;
+
+import dev.linkedlogics.redis.service.RedisContextService;
+import dev.linkedlogics.redis.service.RedisQueueService;
+import dev.linkedlogics.redis.service.RedisTopicService;
+import dev.linkedlogics.redis.service.RedisTriggerService;
+import dev.linkedlogics.service.LinkedLogicsService;
+import dev.linkedlogics.service.ServiceProvider;
+import dev.linkedlogics.service.local.QueueSchedulerService;
+
+public class RedisServices implements ServiceProvider {
+	@Override
+	public List<LinkedLogicsService> getMessagingServices() {
+		return List.of(new RedisQueueService(), new RedisTopicService());
+	}
+
+	@Override
+	public List<LinkedLogicsService> getSchedulingServices() {
+		return List.of(new QueueSchedulerService());
+	}
+
+	@Override
+	public List<LinkedLogicsService> getStoringServices() {
+		return List.of(new RedisContextService(), new RedisTriggerService());
+	}
+}
